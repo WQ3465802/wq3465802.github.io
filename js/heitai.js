6c426471b5454be36629e0b8ac8910ecc3d5ec66
爱爱猫 <WQ3465802@126.COM>
2020-07-26 21:27:30 +0800

    Update heitai.js

--- js/heitai.js
+++ js/heitai.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         nHentai中文标签
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        *nhentai.net/*
